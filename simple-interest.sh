1538ccba963457ccd3f31595c96c4e602d54176d
Manuel Alejandro Londoño Cadavid <49459073+bjergsenson@users.noreply.github.com>
2023-10-19 21:49:17 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Author: Manuel Londoño
+   # Additional Authors: 
+   # Bjergsenson
 
    # Input:
    # p, principal amount
